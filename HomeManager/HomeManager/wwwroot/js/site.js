739b275fded2f52933d36e1df794da89e1b946c9
Herédi Dániel <heredi.daniel@mod.hu>
2020-10-06 14:13:05 +0200

    Munka a layoutokkal.

--- HomeManager/HomeManager/wwwroot/js/site.js
+++ HomeManager/HomeManager/wwwroot/js/site.js
@@ -1,4 +1,8 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+function test() {
+    alert("Még egyenlőre nem értek ehhez, de idővel meglesz!");
+}
+
 // Write your JavaScript code.
